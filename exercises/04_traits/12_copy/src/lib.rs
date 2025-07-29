2a4ac754273ac36db359ad6399e3c2446d132d20
varungupta <thevarungupta@gmail.com>
2025-07-29 16:08:30 +0530

    Completed Traits (12. Copy)

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,9 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +11,16 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        WrappingU32 {
+            value: (self.value + rhs.value),
+        }
     }
 }
 
