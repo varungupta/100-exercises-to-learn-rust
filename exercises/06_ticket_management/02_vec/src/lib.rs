e0ebbee3ddf7ce4981402c9bb4c3c20cba7152af
varungupta <thevarungupta@gmail.com>
2025-09-22 17:23:19 +0530

    Completed Ticket Management (2. Vec)

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -10,12 +10,19 @@
 //
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
+
 pub fn fibonacci(n: u32) -> u32 {
     // TODO: implement the `fibonacci` function
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let n = n as usize;
+    let mut fib: Vec<u32> = vec![0, 1];
+
+    for i in 2..=n {
+        fib.push(fib[i - 2] + fib[i - 1]);
+    }
+    fib[n]
 }
 
 #[cfg(test)]
