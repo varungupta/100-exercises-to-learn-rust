36dfea2c39c78faa5378f38cb8edd0623120a1bf
varungupta <thevarungupta@gmail.com>
2025-09-23 13:27:36 +0530

    Completed Ticket Management (7. Combinators)

--- exercises/06_ticket_management/07_combinators/src/lib.rs
+++ exercises/06_ticket_management/07_combinators/src/lib.rs
@@ -31,6 +31,13 @@ impl TicketStore {
     pub fn add_ticket(&mut self, ticket: Ticket) {
         self.tickets.push(ticket);
     }
+
+    pub fn to_dos(&self) -> std::vec::Vec<&Ticket> {
+        self.tickets
+            .iter()
+            .filter(|&ticket| ticket.status == Status::ToDo)
+            .collect::<Vec<&Ticket>>()
+    }
 }
 
 #[cfg(test)]
