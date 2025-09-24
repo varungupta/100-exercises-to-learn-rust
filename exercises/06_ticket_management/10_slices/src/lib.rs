154d9b493b710ff4de547a6ba9a2473f9ae08721
varungupta <thevarungupta@gmail.com>
2025-09-24 12:24:43 +0530

    Completed Ticket Management (10. Slices)

--- exercises/06_ticket_management/10_slices/src/lib.rs
+++ exercises/06_ticket_management/10_slices/src/lib.rs
@@ -1,6 +1,10 @@
 // TODO: Define a function named `sum` that takes a reference to a slice of `u32` and returns the sum of all
 //  elements in the slice.
 
+fn sum(seq: &[u32]) -> u32 {
+    seq.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
